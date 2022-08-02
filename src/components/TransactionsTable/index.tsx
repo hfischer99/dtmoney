c78a6095fd082d4fb444e72ed48591b5849d55ae
hischer99 <bernardfischer99@hotmail.com>
2022-08-01 23:09:00 -0300

    miragejs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
 export function TransactionsTable(){
+    useEffect(() => {
+        fetch('htpp://localhost:3000/api/transactions')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, [])
     return(
         <Container>
             <table>
